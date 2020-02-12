61f2619706c3e722e73e5f301e2476b20bf6b9fe
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-02-12 05:40:15 -0800

    Stream from a video device to local host
    Device is hard-coded to /dev/video0 at the moment.
    This should be a command line argument later on.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,19 @@ extern crate gstreamer as gst;
 use gst::prelude::*;
 
 fn main() {
+    println!("Initializing pipeline...");
+    let device = "/dev/video0";
+    let ipv4 = "127.0.0.1";
+    let port = "5000";
+
     // Initialize GStreamer
     gst::init().unwrap();
 
     // Create the elements
-    let src = gst::ElementFactory::make("videotestsrc", None)
-        .expect("Could not create source element");
+    let src = gst::ElementFactory::make("v4l2src", None)
+	.expect("Could not create source element");
+    let conv = gst::ElementFactory::make("videoconvert", None)
+	.expect("Could not create source element");
     let enc = gst::ElementFactory::make("x264enc", None)
         .expect("Could not create x264 encoder");
     let pay = gst::ElementFactory::make("rtph264pay", None)
@@ -19,21 +26,24 @@ fn main() {
     let pipeline = gst::Pipeline::new(Some("basic streamer"));
 
     // Build the pipeline
-    pipeline.add_many(&[&src, &enc, &pay, &sink]).unwrap();
-    src.link(&enc).expect("Could not link source to encoder");
+    pipeline.add_many(&[&src, &conv, &enc, &pay, &sink]).unwrap();
+    src.link(&conv).expect("Could not link source to vidoe converter");
+    conv.link(&enc).expect("Could not link video converter to encoder");
     enc.link(&pay).expect("Could not link encoder to RTP payload");
     pay.link(&sink).expect("Could not link encoder to RTP payload");
 
-    // Modify the source's properties
-    sink.set_property_from_str("host", "127.0.0.1");
-    sink.set_property_from_str("port", "5000");
+    // Direct the source to the specified camera device
+    src.set_property_from_str("device", device);
 
-    // Start playing
+    // Direct the sink to our host
+    sink.set_property_from_str("host", ipv4);
+    sink.set_property_from_str("port", port);
+
+    // Start the stream pipeline
+    println!("Starting stream...");
     pipeline
         .set_state(gst::State::Playing)
         .expect("Unable to set the pipeline to the `Playing` state");
-
-    // Wait until error or EOS
     let bus = pipeline.get_bus().unwrap();
     for msg in bus.iter_timed(gst::CLOCK_TIME_NONE) {
         use gst::MessageView;
@@ -55,4 +65,6 @@ fn main() {
     pipeline
         .set_state(gst::State::Null)
         .expect("Unable to set the pipeline to the `Null` state");
+
+    println!("Stream finished");
 }
