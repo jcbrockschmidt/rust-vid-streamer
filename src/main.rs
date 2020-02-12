005b737838ddf988c3aef1deb4e77b0c419de0ab
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-02-12 06:36:46 -0800

    Split main into separate functions

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,7 @@ fn usage(args: Vec<String>) {
     println!("Usage: {} device ipv4 port", args[0]);
 }
 
-fn main() {
-    // Read command line arguments
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 4 {
-	usage(args);
-	process::exit(1);
-    }
-    let device = String::from(&args[1]);
-    let ipv4 = String::from(&args[2]);
-    let port = String::from(&args[3]);
-
-    println!("Initializing pipeline...");
-
-    // Initialize GStreamer
-    gst::init().unwrap();
-
-    // Create the elements
+fn create_pipeline(device: &String, ip: &String, port: &String) -> gst::Pipeline {
     let src = gst::ElementFactory::make("v4l2src", None)
 	.expect("Could not create source element");
     let conv = gst::ElementFactory::make("videoconvert", None)
@@ -45,14 +29,23 @@ fn main() {
     pay.link(&sink).expect("Could not link encoder to RTP payload");
 
     // Direct the source to the specified camera device
-    src.set_property_from_str("device", &device);
+    src.set_property_from_str("device", device);
 
     // Direct the sink to our host
-    sink.set_property_from_str("host", &ipv4);
-    sink.set_property_from_str("port", &port);
+    sink.set_property_from_str("host", ip);
+    sink.set_property_from_str("port", port);
+
+    return pipeline
+}
+
+fn start_stream(device: &String, ip: &String, port: &String) {
+    // Initialize GStreamer and pipeline;
+    println!("Initializing pipeline...");
+    gst::init().unwrap();
+    let pipeline = create_pipeline(device, ip, port);
 
     // Start the stream pipeline
-    println!("Streaming {} to {}:{}...", &device, &ipv4, &port);
+    println!("Streaming {} to {}:{}...", device, ip, port);
     pipeline
         .set_state(gst::State::Playing)
         .expect("Unable to set the pipeline to the `Playing` state");
@@ -77,6 +70,20 @@ fn main() {
     pipeline
         .set_state(gst::State::Null)
         .expect("Unable to set the pipeline to the `Null` state");
+}
+
+fn main() {
+    // Read command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 4 {
+	usage(args);
+	process::exit(1);
+    }
+    let device = String::from(&args[1]);
+    let ipv4 = String::from(&args[2]);
+    let port = String::from(&args[3]);
+
+    start_stream(&device, &ipv4, &port);
 
     println!("Stream finished");
 }
