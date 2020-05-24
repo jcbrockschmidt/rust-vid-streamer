6d6dd6a7039c2674ae8702583037ba22e8b65cd3
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-05-23 20:31:06 -0700

    Implement basic RTSP server utility
    Only shows a test stream on a pre-determined port

---
+++ src/rtsp-server/bin/main.rs
@@ -0,0 +1,44 @@
+extern crate gstreamer as gst;
+use gst::prelude::*;
+use std::{env, process};
+
+extern crate gstreamer_rtsp_server as gst_rtsp_server;
+use gst_rtsp_server::prelude::*;
+
+fn usage(args: Vec<String>) {
+    println!("Usage: {} device port", args[0]);
+}
+
+fn start_stream(device: &String, port: &String) {
+    let server = gst_rtsp_server::RTSPServer::new();
+    let mounts = server.get_mount_points().unwrap();
+    let factory = gst_rtsp_server::RTSPMediaFactory::new();
+    factory.set_launch("( videotestsrc ! x264enc ! rtph264pay name=pay0 pt=96 )");
+    factory.set_shared(true);
+    mounts.add_factory("/stream", &factory);
+
+    let id = server.attach(None);
+    println!(
+        "Stream ready at rtsp://127.0.0.1:{}/stream",
+        server.get_bound_port()
+    );
+    let main_loop = glib::MainLoop::new(None, false);
+    main_loop.run();
+    glib::source_remove(id);
+}
+
+fn main() {
+    // Read command line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        usage(args);
+        process::exit(1);
+    }
+    let device = String::from(&args[1]);
+    let port = String::from(&args[2]);
+
+    gst::init().unwrap();
+    start_stream(&device, &port);
+
+    println!("Stream finished");
+}
